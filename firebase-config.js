70118d9b70434296edfe7cab3e9233803f8c71a4
liam1234569 <liamgartner1234@gmail.com>
2026-04-25 15:11:30 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
-// Deine persönlichen Zugangsdaten von der Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyBN8UBCAZ9Z_eVpVufUn8E_pgu-ygqa_NQ",
   authDomain: "tech-a74d4.firebaseapp.com",
-  databaseURL: "https://tech-a74d4-default-rtdb.europe-west1.firebasedatabase.app", // Aus deinem Screenshot
+  databaseURL: "https://tech-a74d4-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "tech-a74d4",
-  storageBucket: "tech-a74d4.appspot.com",
-  messagingSenderId: "DEINE_ID",
-  appId: "DEINE_APP_ID"
+  storageBucket: "tech-a74d4.firebasestorage.app",
+  messagingSenderId: "111571920796",
+  appId: "1:111571920796:web:700fa92ffa3b7c64d10a59"
 };
+
 
 // Firebase initialisieren
 firebase.initializeApp(firebaseConfig);
